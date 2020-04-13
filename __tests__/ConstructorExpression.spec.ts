96115b550509c664ca775149ac4201e892313374
Joshua Tenner <tenner.joshua@gmail.com>
2020-04-12 23:39:12 -0400

    add more constructor expectations

--- __tests__/ConstructorExpression.spec.ts
+++ __tests__/ConstructorExpression.spec.ts
@@ -7,6 +7,10 @@ import { VisitorContext, visit } from "../src";
 
 const visitor: IVisitorMock = {
   constructorExpression: createVisitorPattern(),
+  declarationStatement: createVisitorPattern(),
+  identifierExpression: createVisitorPattern(),
+  expression: createVisitorPattern(),
+  statement: createVisitorPattern(),
 };
 
 visit(`class A { constructor() {} }`, visitor);
@@ -20,5 +24,25 @@ describe("ConstructorExpression", () => {
     const context: VisitorContext<ConstructorExpression> = call[1];
     expect(context.node).toBe(node);
     expect(node).toBeInstanceOf(ConstructorExpression);
+  });
+
+  test("declarationStatement", () => {
+    expect(visitor.declarationStatement.enter).toBeCalledTimes(2);
+    expect(visitor.declarationStatement.exit).toBeCalledTimes(2);
+  });
+
+  test("identifierExpression", () => {
+    expect(visitor.identifierExpression.enter).toBeCalledTimes(2);
+    expect(visitor.identifierExpression.exit).toBeCalledTimes(2);
+  });
+
+  test("statement", () => {
+    expect(visitor.statement.enter).toBeCalledTimes(3);
+    expect(visitor.statement.exit).toBeCalledTimes(3);
+  });
+
+  test("expression", () => {
+    expect(visitor.expression.enter).toBeCalledTimes(2);
+    expect(visitor.expression.exit).toBeCalledTimes(2);
   });
 });
